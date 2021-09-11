7f6ecbdd41e47b017f0052e6acefd709237441e5
Baptiste Hurel <58144828+BaptisteHurel@users.noreply.github.com>
2021-09-11 13:45:12 +0200

    Add files via upload

---
+++ com.formation.sboot.jpa.films/src/main/java/com/formation/sboot/jpa/films/Application.java
@@ -0,0 +1,13 @@
+package com.formation.sboot.jpa.films;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ com.formation.sboot.jpa.films/src/test/java/com/formation/sboot/jpa/films/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation.sboot.jpa.films;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
